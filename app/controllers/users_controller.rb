4482031faf08665db669e92c3d6ff4fc6f5e7281
Arthur Philadelpho <arthur22rj@gmail.com>
2017-03-15 09:48:59 -0300

    First Attempt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,21 @@ class UsersController < ApplicationController
 			render :new
 		end
 	end
+
+	def confirm 
+		@user = User.new
+		@user.name = params[:user][:name]
+		@user.email = params[:user][:email]
+		@user.password = params[:user][:password]
+		@user.password_confirmation = params[:user][:password_confirmation]
+
+		if @user.save
+			flash[:notice] = "Data confirmed."
+			redirect_to root_path
+		else
+			flash.now[:alert] = "There was an error with your data confirmation."
+			render :new
+		end
+	end
+
 end
