32ae878745c756773105ba9e639f82f9addd1b9b
Arthur Philadelpho <arthur22rj@gmail.com>
2017-03-17 11:46:08 -0300

    Stumbled Upon an Issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
 require 'random_data'
 
+
+5.times do
+	User.create!(
+		name:     RandomData.random_name,
+   	email:    RandomData.random_email,
+   	password: RandomData.random_sentence
+	)
+end
+users = User.all
+
+
 15.times do
 	Topic.create!(
 		name:         RandomData.random_sentence,
@@ -11,6 +22,8 @@ topics = Topic.all
 
 50.times do
   Post.create!(
+  	# user: users.sample,
+  	topic: topics.sample,
   	title:  RandomData.random_sentence,
    	body:   RandomData.random_paragraph
   )
@@ -52,7 +65,16 @@ Question.find_or_create_by!(
 	body: RandomData.random_paragraph
 )
 
+user = User.first
+user.update_attributes!(
+	user.update_attributes!(
+		email: 'arthur22rj@gmail.com',
+		password: 'pass123'
+	)
+)
+
 puts "Seed finished"
+# puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
